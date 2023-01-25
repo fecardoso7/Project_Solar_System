97954b7a0f561301aca40e1d12c4117442019f97
fecardoso7 <felipecardoso1530@gmail.com>
2023-01-25 02:46:14 -0300

    Oitavo requisito

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" className="divMissoes"> </div>
+      <div data-testid="missions" className="divMissoes">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
